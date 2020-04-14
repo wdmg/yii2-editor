380738bfb985b3387d8f1965dd1fe5907d946506
alex-wdmg <alex.vyshnyvetskyy@gmail.com>
2020-04-14 19:32:51 +0300

    Update WYSIWYG editor

--- src/Editor.php
+++ src/Editor.php
@@ -6,7 +6,7 @@ namespace wdmg\widgets;
  * Yii2 WYSIWYG editor
  *
  * @category        Widgets
- * @version         1.0.9
+ * @version         1.0.10
  * @author          Alexsander Vyshnyvetskyy <alex.vyshnyvetskyy@gmail.com>
  * @link            https://github.com/wdmg/yii2-editor
  * @copyright       Copyright (c) 2019 - 2020 W.D.M.Group, Ukraine
